6e4b1a2507dc25b15f7897a00cb486c3dd9442bf
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-07-26 14:33:46 -0500

    Login page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState  } from 'react'
 import Login from './Login'
 
 function App() {
+  const [id, setId] = useState()
+
   return (
-    <Login></Login>
-  );
+    < >
+    {id}
+    <Login onIdSubmit ={setId} />
+    </>
+  ) 
 }
 
 export default App;

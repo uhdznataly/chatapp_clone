62fa507cec0db2a8385f37b74de61794380f339a
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-07-26 13:06:56 -0500

    first commit

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function Login() {
+    return (
+      <Container>
+         Hello Login
+      </Container>
+
+        
+    )
+} 

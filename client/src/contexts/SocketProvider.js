cf8cd41cd302266f090acc0934dfe9a01f5f0055
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-07-26 21:06:04 -0500

    fix issue

--- client/src/contexts/SocketProvider.js
+++ client/src/contexts/SocketProvider.js
@@ -12,7 +12,7 @@ export function SocketProvider({ id, children }) {
 
   useEffect(() => {
     const newSocket = io(
-      'http://localhost:5000',
+      'http://localhost:3000',
       { query: { id } }
     )
     setSocket(newSocket)
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(5000)
+const io = require('socket.io')(3000)
 
 io.on('connection', socket => {
   const id = socket.handshake.query.id
